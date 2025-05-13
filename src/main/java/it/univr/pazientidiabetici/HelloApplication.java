b772407f40dbbdc94724349db7654af0fa3fde7c
FrancescoEsk <francifrison.ff@gmail.com>
2025-05-13 16:25:10 +0200

    Add FXML files for symptom input and update main application layout

--- src/main/java/it/univr/pazientidiabetici/HelloApplication.java
+++ src/main/java/it/univr/pazientidiabetici/HelloApplication.java
@@ -4,14 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.BootstrapFX;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        // prende il file fxml e lo carica
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        // componente principale della applicazione
+        Scene scene = new Scene(fxmlLoader.load());
+
+        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet()); // Carica il foglio di stile
+
+        // stage è la finestra principale
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
