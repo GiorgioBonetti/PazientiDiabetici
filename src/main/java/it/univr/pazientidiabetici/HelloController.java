c39591518e620c64b9239efac8d5e4a0c64b500a
Giorgio Bonetti <78914976+GiorgioBonetti@users.noreply.github.com>
2025-08-12 09:35:19 +0200

    ciao

--- src/main/java/it/univr/pazientidiabetici/HelloController.java
+++
@@ -1,7 +0,0 @@
-package it.univr.pazientidiabetici;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
-public class HelloController {
-}
--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module it.univr.pazientidiabetici {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens it.univr.pazientidiabetici to javafx.fxml;
-    exports it.univr.pazientidiabetici;
-}
